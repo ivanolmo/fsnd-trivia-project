9402447f6485164dff880f8d9fcbdf1e5aa6a444
Ivan Olmo <ivan.olmo@gmail.com>
2020-01-01 19:45:01 -0600

    Add CORS and set access-control-allow headers to app

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -13,13 +13,17 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
+    CORS(app)
 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PUT,POST,PATCH,DELETE,OPTIONS')
     '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-
     '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
 
     '''
